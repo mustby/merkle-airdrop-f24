46944a6dca3508bd1e8a0e9cf152af1e68825cf9
mustby <mjustby@gmail.com>
2025-11-12 10:34:49 -0600

    Updates

--- src/BagelToken.sol
+++ src/BagelToken.sol
@@ -6,7 +6,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BagelToken is ERC20, Ownable {
-    constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender) {}
+    constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender) {} // msg.sender here means that the deployer is the initial owner
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
